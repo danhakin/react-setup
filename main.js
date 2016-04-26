6ab9f5ead5aafb09459172bf4bc3b8dfcca0f1db
Daniel Villalobos <dvillalobos@rockstone.local>
2016-04-26 22:47:47 +0200

    Added a new Title component using propTypes and defaultProps

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import App from './App';
+import Title from './Title';
 
 ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<Title name="Joe" />, document.getElementById('title'))
+
