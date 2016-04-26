48f743885773daeb6a5fd5ab7f0478f914164e6a
Daniel Villalobos <dvillalobos@rockstone.local>
2016-04-26 23:15:43 +0200

    Added state to the Title component

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ import App from './App';
 import Title from './Title';
 
 ReactDOM.render(<App />, document.getElementById('app'))
-ReactDOM.render(<Title name="Joe" />, document.getElementById('title'))
+ReactDOM.render(<Title />, document.getElementById('title'))
 
